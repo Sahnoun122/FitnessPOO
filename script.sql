2d311096ae45fd8bbe7c633ccdcfe5b12e8fac59
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2024-12-25 15:56:55 +0100

    Added SQL scripts in script.sql

--- script.sql
+++ script.sql
@@ -1,63 +1,49 @@
-CREATE DATABASE FitBook;
-USE FitBook;
-
-CREATE TABLE membres (
-    MembreID INT PRIMARY KEY AUTO_INCREMENT,
-    Nom VARCHAR(50),
-    Prenom VARCHAR(50),
-    Telephone VARCHAR(20),
-    Email VARCHAR(100)
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(100),
+    Password VARCHAR(100),
+    Role ENUM('Admin', 'Member')
 );
 
-CREATE TABLE activites (
-    ActiviteID INT PRIMARY KEY AUTO_INCREMENT,
-    Nom VARCHAR(50),
+CREATE TABLE Members (
+    MemberID INT PRIMARY KEY,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    Phone VARCHAR(20) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    FOREIGN KEY (MemberID) REFERENCES Users (UserID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Activities (
+    ActivityID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
     Description TEXT
 );
 
-CREATE TABLE reservations (
-    ReservationID INT PRIMARY KEY AUTO_INCREMENT,
-    MembreID INT,
-    ActiviteID INT,
-    ReservationDate DATETIME,
-    FOREIGN KEY (MembreID) REFERENCES Membres(MembreID),
-    FOREIGN KEY (ActiviteID) REFERENCES Activites(ActiviteID)
+CREATE TABLE Reservations (
+    ResID INT AUTO_INCREMENT PRIMARY KEY,
+    MemberID INT NOT NULL,
+    ActivityID INT NOT NULL,
+    status ENUM('confirmed', 'cancelled', 'pending') DEFAULT 'pending',
+    ResDate DATETIME NOT NULL,
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID),
+    FOREIGN KEY (ActivityID) REFERENCES Activities(ActivityID)
 );
+
 
 -- Example of inserting --
 
-INSERT INTO membres (Nom, Prenom, Telephone, Email) VALUES
-('CHAMKHI', 'Mohammed', '212-636-253939', 'theshamkhi1@gmail.com'),
-('MARSLI', 'Younes', '123-456-789', 'marsli.younes@gmail.com');
 
-INSERT INTO activites (Nom, Description) VALUES
+INSERT INTO Membres (FirstName, LastName, Phone, Email) VALUES
+('Mohammed', 'CHAMKHI', '212-636-253939', 'theshamkhi1@gmail.com'),
+('Khadija', 'SAHNOUN', '123-456-789', 'khadija.sahnoun@gmail.com');
+
+INSERT INTO Activities (Name, Description) VALUES
 ('Yoga', 'A relaxing activity focusing on stretching and mindfulness.'),
 ('Spinning', 'A high-intensity cycling class for cardiovascular fitness.'),
 ('Pilates', 'A low-impact exercise for flexibility, strength, and posture.'),
 ('CrossFit', 'A high-intensity workout combining strength and cardio exercises.');
 
-INSERT INTO reservations (MembreID, ActiviteID, ReservationDate) VALUES 
+INSERT INTO Reservations (MemberID, ActivityID, ResDate) VALUES 
 (1, 1, '2024-12-15 09:00:00'),
 (2, 2, '2024-12-16 10:30:00');
-
-
--- Example of modifying --
-
-UPDATE activites 
-SET Description = 'An intensive cycling class designed to improve stamina.'
-WHERE ActiviteID = 2;
-
-UPDATE reservations 
-SET ReservationDate = '2024-12-17 11:00:00'
-WHERE ReservationID = 2;
-
--- Example of deleting --
-
-DELETE FROM membres 
-WHERE MembreID = 2;
-
-DELETE FROM activites 
-WHERE ActiviteID = 4;
-
-DELETE FROM reservations 
-WHERE ReservationID = 1;
