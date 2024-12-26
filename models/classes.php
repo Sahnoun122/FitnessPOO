bc310a972318e4d54d5b05d1fd22aff8cea41fbf
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2024-12-26 16:00:54 +0100

    Updated models

--- models/classes.php
+++ models/classes.php
@@ -143,11 +143,13 @@ class Member extends User {
 class Activity {
     private $id;
     private $name;
+    private $image;
     private $description;
 
-    public function __construct($id, $name, $description) {
+    public function __construct($id, $name, $image, $description) {
         $this->id = $id;
         $this->name = $name;
+        $this->image = $image;
         $this->description = $description;
     }
 
@@ -157,12 +159,18 @@ class Activity {
     public function getName() {
         return $this->name;
     }
+    public function getImg() {
+        return $this->image;
+    }
     public function getDescription() {
         return $this->description;
     }
 
     public function setName($name) {
         $this->name = $name;
+    }
+    public function setImg() {
+        $this->image = $image;
     }
     public function setDescription($description) {
         $this->description = $description;
