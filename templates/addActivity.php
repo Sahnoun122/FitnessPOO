3378115a0d6b38b6b406e34ba67de4e32d3d2d10
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2024-12-27 01:38:10 +0100

    Enhanced UI

--- templates/addActivity.php
+++ templates/addActivity.php
@@ -113,9 +113,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_activity_id'])
       <ul class="space-y-2 font-medium px-3 pb-4">
         <li>
             <a href="adminDashboard.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                  <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
-               </svg>
+                <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                </svg>
                <span class="ms-3">Dashboard</span>
             </a>
         </li>
@@ -142,6 +143,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_activity_id'])
 
 <!-- Main -->
 <div class="p-8 sm:ml-80">
+
+    <h2 class="text-4xl font-semibold text-black mb-10">Activities</h2>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-12" style="align-items: start;">
+        <?php
+            $activities_sql = "SELECT * FROM Activities";
+            $stmt_activities = $pdo->query($activities_sql);
+            $activities = $stmt_activities->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($activities as $activity):
+        ?>
+        <div class="bg-white shadow-lg rounded-lg overflow-hidden" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+            <img src="<?php echo $activity['PhotoURL']; ?>" alt="Activity Photo" class="w-full h-48 object-cover">
+            <div class="p-6">
+                <h3 class="text-4xl mb-4 font-semibold text-gray-900"><?php echo $activity['Name']; ?></h3>
+                <p class="text-lg text-gray-700"><?php echo $activity['Description']; ?></p>
+                <form method="POST" onsubmit="return confirm('Are you sure you want to delete this activity?');">
+                    <input type="hidden" name="delete_activity_id" value="<?php echo $activity['ActivityID']; ?>">
+                    <div class="flex items-center justify-center mt-4">
+                        <button type="submit" class="text-xl hover:scale-105">🗑️</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <?php endforeach; ?>
+
+    </div>
 
     <h2 class="text-4xl font-semibold text-black mb-6">Add New Activity</h2>
     <div class="flex items-center justify-center my-8 bg-gray-100">
@@ -178,33 +206,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_activity_id'])
 
             </div>
         </div>
-    </div>
-
-    <h2 class="text-4xl font-semibold text-black mb-10">Activities</h2>
-
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4" style="align-items: start;">
-        <?php
-            $activities_sql = "SELECT * FROM Activities";
-            $stmt_activities = $pdo->query($activities_sql);
-            $activities = $stmt_activities->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($activities as $activity):
-        ?>
-        <div class="bg-white shadow-lg rounded-lg overflow-hidden" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-            <img src="<?php echo $activity['PhotoURL']; ?>" alt="Activity Photo" class="w-full h-48 object-cover">
-            <div class="p-6">
-                <h3 class="text-4xl mb-4 font-semibold text-gray-900"><?php echo $activity['Name']; ?></h3>
-                <p class="text-lg text-gray-700"><?php echo $activity['Description']; ?></p>
-                <form method="POST" onsubmit="return confirm('Are you sure you want to delete this activity?');">
-                    <input type="hidden" name="delete_activity_id" value="<?php echo $activity['ActivityID']; ?>">
-                    <div class="flex items-center justify-center mt-4">
-                        <button type="submit" class="text-xl hover:scale-105">🗑️</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <?php endforeach; ?>
-
     </div>
 
 </div>
